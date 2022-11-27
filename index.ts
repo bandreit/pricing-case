f524a4c1b5c427d6456a169b5e9b553e05a0fb82
bandreit <andy27hush@gmail.com>
2022-11-27 15:03:18 +0100

    add a seed endpoint

--- index.ts
+++ index.ts
@@ -1,53 +1,111 @@
 // Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as aws from '@pulumi/aws';
+import * as awsx from '@pulumi/awsx';
 
 // Create a mapping from 'route' to a count
-const counterTable = new aws.dynamodb.Table("counterTable", {
-    attributes: [{
-        name: "id",
-        type: "S",
-    }],
-    hashKey: "id",
-    readCapacity: 5,
-    writeCapacity: 5,
+const productTable = new aws.dynamodb.Table('Product', {
+  attributes: [
+    { name: 'pk', type: 'S' },
+    { name: 'sk', type: 'S' },
+  ],
+  hashKey: 'pk',
+  rangeKey: 'sk',
+  readCapacity: 5,
+  writeCapacity: 5,
 });
 
+interface ProductPriceDTO {
+  id: string;
+  name: string;
+  region: string;
+  currency: string;
+  centValue: number;
+  validFrom: string;
+  validTo?: string;
+}
+
+const eifelDK: ProductPriceDTO = {
+  id: '10307',
+  name: 'Eifel Tower',
+  region: 'DK',
+  currency: 'DKK',
+  centValue: 489900,
+  validFrom: '1668466800',
+};
+
+const eifelUS: ProductPriceDTO = {
+  id: '10307',
+  name: 'Eifel Tower',
+  region: 'US',
+  currency: 'USD',
+  centValue: 62999,
+  validFrom: '1668466800',
+};
+
+const titanicDK: ProductPriceDTO = {
+  id: '10294',
+  name: 'Eifel Tower',
+  region: 'DK',
+  currency: 'DKK',
+  centValue: 529900,
+  validFrom: '1668466800',
+};
+
+const productsForSeeding = [eifelDK, eifelUS, titanicDK];
+
 // Create an API endpoint
-const endpoint = new awsx.apigateway.API("hello-world", {
-    routes: [{
-        path: "/{route+}",
-        method: "GET",
-        eventHandler: async (event) => {
-            const route = event.pathParameters!["route"];
-            console.log(`Getting count for '${route}'`);
+const endpoint = new awsx.apigateway.API('pricing', {
+  routes: [
+    {
+      path: '/seed',
+      method: 'POST',
+      eventHandler: async (event) => {
+        let result;
+        try {
+          const client = new aws.sdk.DynamoDB.DocumentClient();
+          const talbeName = productTable.name.get();
 
-            const client = new aws.sdk.DynamoDB.DocumentClient();
-
-            // get previous value and increment
-            // reference outer `counterTable` object
-            const tableData = await client.get({
-                TableName: counterTable.name.get(),
-                Key: { id: route },
-                ConsistentRead: true,
-            }).promise();
-
-            const value = tableData.Item;
-            let count = (value && value.count) || 0;
-
-            await client.put({
-                TableName: counterTable.name.get(),
-                Item: { id: route, count: ++count },
-            }).promise();
-
-            console.log(`Got count ${count} for '${route}'`);
+          const productDocs = productsForSeeding.map((product) => {
             return {
-                statusCode: 200,
-                body: JSON.stringify({ route, count }),
+              pk: product.id,
+              sk: product.region,
+              region: product.region,
+              currency: product.currency,
+              centValue: product.centValue,
+              validFrom: product.validFrom,
             };
-        },
-    }],
+          });
+
+          const putRequests = productDocs.map((productDoc) => {
+            return {
+              PutRequest: {
+                Item: productDoc,
+              },
+            };
+          });
+
+          result = await client
+            .batchWrite({
+              RequestItems: {
+                [talbeName]: putRequests,
+              },
+            })
+            .promise();
+        } catch (error) {
+          return {
+            statusCode: error.statusCode,
+            body: JSON.stringify(error),
+          };
+        }
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ result }),
+        };
+      },
+    },
+  ],
 });
 
 exports.endpoint = endpoint.url;
