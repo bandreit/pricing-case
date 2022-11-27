c048e78c7f9e656566268d5676eb83e7a4c08b1a
bandreit <andy27hush@gmail.com>
2022-11-27 19:54:55 +0100

    filter prices of product by region

--- handlers/get-prices.ts
+++ handlers/get-prices.ts
@@ -12,20 +12,37 @@ export const productPricesHandler = async (event: APIGatewayProxyEvent) => {
   try {
     const client = new aws.sdk.DynamoDB.DocumentClient();
     const productId = event.pathParameters!['productId'];
+    const region = event.queryStringParameters?.region;
+
     if (productId == null) {
       throw new Error('No product was specified');
     }
 
-    const { Items = [], Count } = await client
-      .query({
+    let queryObject;
+
+    queryObject = {
+      TableName: productTable.name.get(),
+      KeyConditionExpression: '#pk = :pk',
+      ExpressionAttributeNames: { '#pk': 'pk' },
+      ExpressionAttributeValues: {
+        ':pk': productId,
+      },
+    };
+
+    if (region !== null) {
+      queryObject = {
         TableName: productTable.name.get(),
         KeyConditionExpression: '#pk = :pk',
-        ExpressionAttributeNames: { '#pk': 'pk' },
+        FilterExpression: '#region = :region',
+        ExpressionAttributeNames: { '#pk': 'pk', '#region': 'region' },
         ExpressionAttributeValues: {
           ':pk': productId,
+          ':region': region,
         },
-      })
-      .promise();
+      };
+    }
+
+    const { Items = [], Count } = await client.query(queryObject).promise();
 
     const products = Items?.map((item) => {
       let newProduct: ProductPriceDTO = {
